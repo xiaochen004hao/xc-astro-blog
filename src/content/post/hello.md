e289c541985d4fdf6099e0468c99839fc1c5946f
xiaochen004hao <xiaochen004hao@163.com>
2026-04-18 21:30:13 +0800

    init cactus blog

---
+++ src/content/post/hello.md
@@ -0,0 +1,8 @@
+---
+title: 我的第一篇文章
+description: 测试一下
+publishDate: 2026-04-18
+tags: ["astro"]
+---
+
+Hello world 🚀
--- src/site.config.ts
+++ src/site.config.ts
@@ -3,18 +3,18 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://astro-cactus.chriswilliams.dev/",
+	url: "https://blog.qoslzm.dpdns.org/",
 	/*
 		- Used to construct the meta title property found in src/components/BaseHead.astro L:11
 		- The webmanifest name found in astro.config.ts L:42
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Astro Cactus",
+	title: "Xiaochen004hao's Blog",
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "xiaochen004hao",
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "xiaochen004hao's blog",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
