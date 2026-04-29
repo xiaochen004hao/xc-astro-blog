7a873fe441373a518e48be6ec73e12d6d34876a8
xiaochen004hao <xiaochen004hao@163.com>
2026-04-29 21:31:46 +0800

    小更新

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,6 @@ import tailwind from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
-// @ts-expect-error
 import robotsTxt from "astro-robots-txt";
 import webmanifest from "astro-webmanifest";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
